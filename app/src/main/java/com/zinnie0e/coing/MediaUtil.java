0bd90ba15600db7334fb3155de9187732363ec79
zinnie0e <zinnie0e@naver.com>
2022-04-07 00:39:08 +0900

    Chore: edit UI-Main, Home #008
    - bottom menu 추가
    - home fragment UI 구현
    - 타이틀바 삭제

---
+++ app/src/main/java/com/zinnie0e/coing/MediaUtil.java
@@ -0,0 +1,115 @@
+package com.zinnie0e.coing;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.speech.RecognitionListener;
+import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
+import android.speech.tts.TextToSpeech;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.annotation.RequiresApi;
+
+import java.util.ArrayList;
+import java.util.Locale;
+
+public class MediaUtil implements View.OnClickListener{
+    Context mContext;
+
+    SpeechRecognizer mRecognizer;
+
+    public MediaUtil(Context context){
+        mContext = context;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()){
+            /*case R.id.btnVoice:
+                //객체에 Context와 listener를 할당한 후 실행
+                mRecognizer = SpeechRecognizer.createSpeechRecognizer((MainActivity) mContext);
+                mRecognizer.setRecognitionListener(listener);
+                mRecognizer.startListening(MainActivity.intent);
+                break;*/
+        }
+    }
+
+    /** STT */
+    //RecognizerIntent 객체에 할당할 listener 생성
+    private RecognitionListener listener = new RecognitionListener() {
+        @Override public void onReadyForSpeech(Bundle params) {
+            Toast.makeText((MainActivity) mContext,"음성인식을 시작합니다.",Toast.LENGTH_SHORT).show();
+        }
+        @Override public void onBeginningOfSpeech() {}
+        @Override public void onRmsChanged(float rmsdB) {}
+        @Override public void onBufferReceived(byte[] buffer) {}
+        @Override public void onEndOfSpeech() {}
+        @Override public void onError(int error) {
+            String message;
+            switch (error) {
+                case SpeechRecognizer.ERROR_AUDIO:
+                    message = "오디오 에러";
+                    break;
+                case SpeechRecognizer.ERROR_CLIENT:
+                    message = "클라이언트 에러";
+                    break;
+                case SpeechRecognizer.ERROR_INSUFFICIENT_PERMISSIONS:
+                    message = "퍼미션 없음";
+                    break;
+                case SpeechRecognizer.ERROR_NETWORK:
+                    message = "네트워크 에러";
+                    break;
+                case SpeechRecognizer.ERROR_NETWORK_TIMEOUT:
+                    message = "네트웍 타임아웃";
+                    break;
+                case SpeechRecognizer.ERROR_NO_MATCH:
+                    message = "찾을 수 없음";
+                    break;
+                case SpeechRecognizer.ERROR_RECOGNIZER_BUSY:
+                    message = "RECOGNIZER가 바쁨";
+                    break;
+                case SpeechRecognizer.ERROR_SERVER:
+                    message = "서버가 이상함";
+                    break;
+                case SpeechRecognizer.ERROR_SPEECH_TIMEOUT:
+                    message = "말하는 시간초과";
+                    break;
+                default:
+                    message = "알 수 없는 오류임";
+                    break;
+            }
+            Toast.makeText((MainActivity) mContext, "에러가 발생하였습니다. : " + message,Toast.LENGTH_SHORT).show();
+        }
+        @Override public void onResults(Bundle results) {
+            //말을 하면 ArrayList에 단어를 넣고 textView에 단어를 이어줍니다.
+            ArrayList<String> matches = results.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
+            for(int i = 0; i < matches.size() ; i++){
+                ((MainActivity) mContext).txtVoice.setText(matches.get(i));
+            }
+        }
+        @Override public void onPartialResults(Bundle partialResults) {}
+        @Override public void onEvent(int eventType, Bundle params) {}
+    };
+
+    /** TTS */
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    public static void speakOut(String conv) {
+        Log.i("!---", conv);
+        CharSequence text = conv;
+        MainActivity.tts.setPitch((float) 0.6);
+        MainActivity.tts.setSpeechRate((float) 0.1);
+        MainActivity.tts.speak(text, TextToSpeech.QUEUE_FLUSH,null,"id1");
+    }
+
+    static void stopSpeak(){
+        if (MainActivity.tts != null) {
+            MainActivity.tts.stop();
+            MainActivity.tts.shutdown();
+        }
+    }
+
+}
---
+++ app/src/main/java/com/zinnie0e/coing/fragment/HomeFragment.java
@@ -0,0 +1,154 @@
+package com.zinnie0e.coing.fragment;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.zinnie0e.coing.ConvData;
+import com.zinnie0e.coing.MainActivity;
+import com.zinnie0e.coing.MediaUtil;
+import com.zinnie0e.coing.MyAdapter;
+import com.zinnie0e.coing.R;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Random;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link HomeFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class HomeFragment extends Fragment {
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+    private String URL = "http://www.englishspeak.com/ko/english-phrases";
+
+    TextView txtAllCount;
+    ListView listView;
+
+    Context mContext;
+    MyAdapter myAdapter;
+    ArrayList<ConvData> convDataList;
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public HomeFragment(Context context) {
+        mContext = context;
+    }
+
+    public HomeFragment() {
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment HomeFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static HomeFragment newInstance(String param1, String param2) {
+        HomeFragment fragment = new HomeFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        crawlingThread();
+
+        listView = (ListView)view.findViewById(R.id.listConv);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView parent, View v, int position, long id){
+                MediaUtil.speakOut(myAdapter.getItem(position).getConv_en());
+            }
+        });
+
+        txtAllCount = (TextView)view.findViewById(R.id.txtAllCount);
+
+        return view;
+    }
+
+    private void crawlingThread() {
+        final Bundle bundle = new Bundle();
+        new Thread(() -> {
+            Document doc = null;
+            try{
+                doc = Jsoup.connect(URL).get();	//URL 웹사이트에 있는 html 코드를 다 끌어오기
+                Elements elements = doc.select(".test"); //원하는 태그만 찾아서 가져오기
+
+                Random rand = new Random();
+                String[] conv = new String[20];
+                for(int i = 0 ; i < 20 ; i++) {
+                    if(!elements.get(rand.nextInt(elements.size())).text().isEmpty()){
+                        conv[i] = elements.get(rand.nextInt(elements.size())).text();
+                        //Log.i("!---", conv[i]);
+                    }
+                }
+                //한글 가져오기 진행중
+                bundle.putStringArray("conv", conv);
+                Message msg = handler.obtainMessage();
+                msg.setData(bundle);
+                handler.sendMessage(msg);
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+        }).start();
+    }
+
+    Handler handler = new Handler() {
+        @Override
+        public void handleMessage(@NonNull Message msg) {
+            Bundle bundle = msg.getData();    //new Thread에서 작업한 결과물 받기
+
+            convDataList = new ArrayList<ConvData>();
+            for(int i = 0; i < bundle.getStringArray("conv").length; i++){
+                convDataList.add(new ConvData(bundle.getStringArray("conv")[i], "(ko)" + bundle.getStringArray("conv")[i]));
+            }
+
+            Log.i("@@@@@", convDataList.size() + "");
+
+            txtAllCount.setText(convDataList.size()+"개");
+            myAdapter = new MyAdapter((MainActivity) mContext, convDataList);
+            listView.setAdapter(myAdapter);
+        }
+    };
+}
