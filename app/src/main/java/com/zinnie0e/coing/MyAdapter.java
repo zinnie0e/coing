6a1e46464ba3a78bfbf8c5529fbe096e4210e0b5
zinnie0e <zinnie0e@naver.com>
2022-04-07 00:31:40 +0900

    ReFactor: make MediaUtil #007
    stt, tts 기능 클래스 분리

--- app/src/main/java/com/zinnie0e/coing/MyAdapter.java
+++ app/src/main/java/com/zinnie0e/coing/MyAdapter.java
@@ -14,6 +14,7 @@ public class MyAdapter extends BaseAdapter {
     Context mContext = null;
     LayoutInflater mLayoutInflater = null;
     ArrayList<ConvData> conv_data;
+    Boolean isEnglish = true;
 
     public MyAdapter(Context context, ArrayList<ConvData> data) {
         mContext = context;
@@ -38,14 +39,22 @@ public class MyAdapter extends BaseAdapter {
 
     @Override
     public View getView(int position, View converView, ViewGroup parent) {
-        View view = mLayoutInflater.inflate(R.layout.listview, null);
+        View view = mLayoutInflater.inflate(R.layout.view_contents_list, null);
 
         TextView txtConvEn = (TextView)view.findViewById(R.id.txtConvEn);
-        TextView txtConvKo = (TextView)view.findViewById(R.id.txtConvKo);
+        TextView txtStatus = (TextView)view.findViewById(R.id.txtStatus);
+        TextView txtIsLook = (TextView)view.findViewById(R.id.txtIsLook);
 
         txtConvEn.setText(conv_data.get(position).getConv_en());
-        txtConvKo.setText(conv_data.get(position).getConv_ko());
 
+        //btnLook 클릭했을때 기능 추가
+        txtIsLook.setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View v) {
+                isEnglish = !isEnglish;
+                if(isEnglish) txtIsLook.setText("🕶");
+                else txtIsLook.setText("\uD83D\uDC40");
+            }
+        });
         return view;
     }
 }
